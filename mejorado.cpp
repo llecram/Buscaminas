3b551f2e095586348fbd633f28146f706a7dff06
Dennis <31170537+llecram@users.noreply.github.com>
2017-09-30 15:29:51 -0500

    Update mejorado.cpp

--- mejorado.cpp
+++ mejorado.cpp
@@ -1,10 +1,17 @@
 #include <windows.h>
+#include<stdlib.h>
 #include <iostream>
 using namespace std;
 int matriz(int,int);
-int bombas(char **x,int a, int b){
-
-
+void agregaBombas(char **z,int nbombas,int a,int b){
+    int i,x,y;// contador de bombas
+    i = 1;
+    while( i <= nbombas){
+        x = rand()%a;
+        y = rand()%b;
+        z[x][y] = '*';
+        i++;
+    }
 }
 int navegacion(char **z,int a, int b){
     int g,u;
@@ -23,7 +30,13 @@ int navegacion(char **z,int a, int b){
         }
     for (int i=0;i<a;i++){
         for(int j=0;j<b;j++){
-            cout<<" "<< z[i][j]<<" ";
+            if(z[i][j]=='*'){
+                break;
+            }
+            else{
+                cout<<" "<< z[i][j]<<" ";
+            }
+
 
         }
         cout<<"\n"<<"\n";
@@ -47,8 +60,9 @@ int matriz(int a, int b){
         }
         cout<<"\n"<<"\n";
     }*/
-
+    agregaBombas(ary,3,a,b);
     navegacion(ary,a,b);
+
     for (int i=0;i<a;i++){
         delete []ary[i];
     delete []ary;
@@ -56,17 +70,20 @@ int matriz(int a, int b){
     return 0;
 }
 int main() {
-int a,b;
+int a,b,bomba;
 bool z=true;
 int N,M;
 cout<<"ingrese numero de filas:"<<" ";
 cin>>N;
 cout<<"ingres el numero de columnas:"<<" ";
 cin>>M;
+cout<<"ingrese el numero de bombas:"<<" ";
+cin>>bomba;
     while(z){
+
             matriz(N,M);
+
     }
 
   return 0;
 }
-
